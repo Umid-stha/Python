afcdb6b64cb010c2f3763292563ad8c75606b183
Umid-stha <137206081+Umid-stha@users.noreply.github.com>
2024-06-14 15:35:54 +0000

    slot machine print complete

--- Python/slotMachine.py
+++ Python/slotMachine.py
@@ -1,8 +1,62 @@
-
+import random
 from curses.ascii import isdigit
 MAX_LINES=3
 MAX_BET=100
 MIN_BET=1
+
+ROWS=3
+COLS=3
+
+symbolCount={
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbolValue={
+    "A": 5,
+    "B": 3,
+    "C": 2,
+    "D": 1.2
+}
+
+def checkWinnings(columns, lines, bet, value):
+    Winnings = 0
+    for line in range(lines):
+        symbol=columns[0][line]
+        for column in columns:
+            symbolToCheck=column[line]
+            if symbol != symbolToCheck:
+                break
+        else:
+            Winnings += value[symbol] * bet
+    return Winnings
+
+def getSlotMachineSpin(rows, cols, symbols):
+    allSymbols = []
+    for symbol, symbolCount in symbols.items():
+        for i in range(symbolCount):
+            allSymbols.append(symbol)
+    columns= []
+    for col in range(cols):
+        column=[]
+        currentSymbol=allSymbols[:]
+        for row in range(rows):
+            value = random.choice(currentSymbol)
+            currentSymbol.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
+
+def printSlotMacine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i!=len(columns)-1:
+                print(column[row], "|", end=" ")
+            else :
+                print(column[row])
+        
 
 
 def deposit():
@@ -55,5 +109,11 @@ def main():
         else:
             break
     print(f"You are betting ${bet} on {lines} lines, Your total bet is ${totalBet}")
+    print("here is the slot machine: ")
+    slots=getSlotMachineSpin(ROWS, COLS, symbolCount)
+    printSlotMacine(slots)
+    winning=checkWinnings(slots, lines, bet, symbolValue)
+    print(f"you have won ${winning} from this roll")
+
 
 main()
